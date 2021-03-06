fd4060d3db06a6d5b6511243a12051245bc4d958
ajonaed <ajonaed@rocketmail.com>
2021-03-05 21:05:41 -0500

    Merge Two List

--- mergeSortedList.py
+++ mergeSortedList.py
@@ -4,7 +4,10 @@ Merge both list into one'''
 def mergeSortedList(list_1, list_2):
     result = []
     i,j = 0,0
-
+    if not list_1:
+        return list_2
+    if not list_2:
+        return list_1
     while ( i<len(list_1) and j<len(list_2)) :
         if list_1[i] <= list_2[j]:
             result.append(list_1[i])
@@ -15,16 +18,16 @@ def mergeSortedList(list_1, list_2):
     if i <len(list_1):
         result.append(list_1[i])
         i += 1
-    if j < len(list_2):
+    elif j < len(list_2):
         result.append(list_2[j])
         j += 1
     return result
 
 
-
 def main():
     list_1 = [0,3,5,31]
-    list_2 = [1,6,9,31]
+    list_2 = [1,6,9,28]
+    
     print(mergeSortedList(list_1,list_2))
 
 
