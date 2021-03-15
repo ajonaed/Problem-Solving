972323e1ab14efc467628a3e6b1f71c525e2a7b5
ajonaed <ajonaed@rocketmail.com>
2021-03-14 23:37:29 -0400

    Recursion Updated

---
+++ Recursion/Main.py
@@ -0,0 +1,27 @@
+# def inception(counter):
+#     if counter > 3:
+#         return counter
+#     else:
+#         # inception(counter+1) # Return None, as First Call of the function doesn't have anything.
+#         return inception(counter+1) + 1  # we need to return the value of last function's return value
+holder = [1]
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return factorial(n-1) * n
+def factorialNonRecursive(n):
+    answer = 1
+    for i in range(n,0,-1):
+        answer *= i
+    return answer
+
+def main():
+    # print(inception(0))
+    print('Recursively: ',factorial(5))
+    print('Non-recursively: ',factorialNonRecursive(5))
+
+
+if __name__ == '__main__':
+    main()
